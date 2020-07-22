8fb83db3ed38f0e192d9e14d63692eca94789061
Kanthorn1995 <you@example.comkanthorn.wo@rmuti.ac.th>
2020-07-22 23:30:42 +0700

    new

--- 3.3.c
+++ 3.3.c
@@ -1,6 +1,22 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+int digitSum(int a ,int *b)
+{
+    int c;
+    while(a!=0){
+        c=a%10;
+        *b +=c;
+        a/=10;
+    }
+    return b;
 
-main(){
-    printf("Hello Wrold");
-    printf("555");
+}
+int main()
+{
+    int num;
+    int sum = 0 ;
+    scanf("%d",&num);
+    digitSum(num,&sum);
+    printf("%d",sum);
+
 }
