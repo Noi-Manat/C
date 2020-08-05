85797a5efa7bbee1559f21f1aa8c818c6870d28a
Kanthorn1995 <you@example.comkanthorn.wo@rmuti.ac.th>
2020-08-06 01:17:27 +0700

    ยังติดบัคอยู้

--- String49.c
+++ String49.c
@@ -1,34 +1,33 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
 int main()
 {
-    char str1[20],str2[20];
-    int count=0;
-    printf("Input Sring[n1] : ");
-    gets(str1);
-    printf("Input Sring[n2] : ");
-    gets(str2);
-    for(int i= 0; i<str1[i]; i++)
-    {
+    char string1[20];
+    char string2[20];
+    int count;
+    printf("Input String[n1] : ");
+    gets(string1);
+    printf("Input String[n2] : ");
+    gets(string2);
+    
+    
+    for(int i = 0; i < strlen(string1); i++) {
         count = 1;
-        for(int j= i+1; j<str2[j]; j++)
-        {
-           //printf("[%d][i]%c\n",i,str1[i]);
-            //printf("[%d][j] %c\n",j,str2[j]);
-            if(str1[i]==str2[j]){
+        for(int j = i; j < strlen(string2); j++) {
+                //printf("%c %c [i]=%d[j]=%d\n",string1[i],string2[j],i,j);
+            if(string1[i] == string2[j] && string1[i] != ' ') {
                 count++;
-                str2[j]=0;
 
 
+                
+                string2[j] = '0';
             }
-
         }
- if(count > 1 && str1[i] != '0')
-           printf("%c = %d\n",str1[i],count);
-
+        
+        if(count > 1 && string1[i] != '0')
+            printf("%c = %d\n", string1[i],count/2);
     }
 
-
-
+    return 0;
 }
